8d650107c6cc1945a88634d3b507f9513401f3a7
tsukimidori <kohei.mizoue17@gmail.com>
2021-09-24 15:46:38 +0900

    正規表現の適用等、細かなバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,18 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :item_name,          presence: true
   validates :item_text,          presence: true
-  validates :category_id,        presence: true
-  validates :item_status_id,     presence: true
-  validates :shipping_charge_id, presence: true
-  validates :prefecture_id,      presence: true
-  validates :required_day_id,    presence: true
+  validates :category_id,        presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_status_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :required_day_id,    presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price,              presence: true
 
   belongs_to :user
   has_one_attached :image
   belongs_to :category, :item_status, :shipping_charge, :prefecture, :required_day
+
+  with_options :format { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters"} do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
 end
