1ce7f393dc2c3027054a4c9feba6a6f88df1eca0
tsukimidori <kohei.mizoue17@gmail.com>
2021-09-24 11:35:25 +0900

    item・user両モデルの設定（アソシエーション）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  validates :item_name,          presence: true
+  validates :item_text,          presence: true
+  validates :category_id,        presence: true
+  validates :item_status_id,     presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id,      presence: true
+  validates :required_day_id,    presence: true
+  validates :price,              presence: true
+
+  belongs_to :user
+  has_one_attached :image
+
 end
