67fb2973809e3b0072d40213f3cb61ec66d94032
tsukimidori <kohei.mizoue17@gmail.com>
2021-09-24 12:42:32 +0900

    各ActiveHashモデルの設定とitemモデルとのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   validates :item_name,          presence: true
   validates :item_text,          presence: true
   validates :category_id,        presence: true
@@ -10,5 +11,5 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-
+  belongs_to :category, :item_status, :shipping_charge, :prefecture, :required_day
 end
