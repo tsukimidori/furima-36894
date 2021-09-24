9214d65316067e889549f856660e4ee6fee177f3
tsukimidori <kohei.mizoue17@gmail.com>
2021-09-24 20:25:41 +0900

    createアクションの設定とitemモデルのバリデーション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_text, :category_id, :item_status_id, :shipping_charge_id, :prefecture_id, :required_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
