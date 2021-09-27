008b2eebb73c4311b029ccd0fde462710a94d995
tsukimidori <kohei.mizoue17@gmail.com>
2021-09-27 18:06:05 +0900

    再リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,8 +44,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    if current_user.id != item.user_id
+    if current_user.id != @item.user_id
       redirect_to root_path
     end
   end
