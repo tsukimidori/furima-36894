41a6e6e478060ad0dfb5a48e457dfdeb80a31f72
tsukimidori <kohei.mizoue17@gmail.com>
2021-09-27 14:24:17 +0900

    item/showのビュー遷移までの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :item_text, :category_id, :item_status_id, :shipping_charge_id, :prefecture_id, :required_day_id, :price, :image).merge(user_id: current_user.id)
