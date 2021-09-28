acc029b79ba04109a3b20868d3fd4a11a05f34a4
tsukimidori <kohei.mizoue17@gmail.com>
2021-09-28 16:54:04 +0900

    モデルのアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  has_one :purchase_record
   belongs_to :category
   belongs_to :item_status
   belongs_to :shipping_charge
