a24346d81fb2550c651a75ed1ff83fb4549ca163
tsukimidori <kohei.mizoue17@gmail.com>
2021-09-28 19:20:37 +0900

    orders/indexビューの変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,17 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
 
   def index
+    @item = Item.find(params[:item_id])
+    @order = RecordAddress.new
+  end
+
+  def create
+    @order = RecordAddress.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,5 @@
 class ShippingAddress < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :purchase_record
+  belongs_to :prefecture
 end
