4f9ca629f3ecfc5c55d1166b5a30468a5ba7c2b4
tsukimidori <kohei.mizoue17@gmail.com>
2021-10-01 14:18:16 +0900

    エラー文を日本語に変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,11 @@ class Item < ApplicationRecord
   validates :image,              presence: true
   validates :item_name,          presence: true
   validates :item_text,          presence: true
-  validates :category_id,        presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :item_status_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_charge_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id,      presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :required_day_id,    presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id,        presence: true, numericality: { other_than: 1, message: "を選択してください" }
+  validates :item_status_id,     presence: true, numericality: { other_than: 1, message: "を選択してください" }
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 1, message: "を選択してください" }
+  validates :prefecture_id,      presence: true, numericality: { other_than: 1, message: "を選択してください" }
+  validates :required_day_id,    presence: true, numericality: { other_than: 1, message: "を選択してください" }
   validates :price,              presence: true
 
   belongs_to :user
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :required_day
 
-  with_options format: { with: /\A[0-9]+\d\z/, message: "is invalid. Input half-width characters"} do
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range"}
+  with_options format: { with: /\A[0-9]+\d\z/, message: "は半角数字で入力してください"} do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "が価格制限の範囲外です"}
   end
 end
--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -3,11 +3,11 @@ class RecordAddress
   attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building_name, :tel_num, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は例のように入力してください (例：123-4567)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
     validates :municipalities
     validates :address
-    validates :tel_num, format: {with: /\A\d{10,11}\z/, message: "is too short"}, numericality: { only_integer: true, message: "is invalid. Input only number"}
+    validates :tel_num, format: {with: /\A\d{10,11}\z/, message: "は10~11桁で入力してください"}, numericality: { only_integer: true, message: "は半角数字のみで入力してください"}
     validates :user_id
     validates :item_id
     validates :token
